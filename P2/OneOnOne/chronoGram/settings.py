8a182dd0a822c45a40defe4d1005607894d5969a
silvialpz <74107990+silvialpz@users.noreply.github.com>
2024-04-02 21:01:13 -0400

    removing silvia email

--- P2/OneOnOne/chronoGram/settings.py
+++ P2/OneOnOne/chronoGram/settings.py
@@ -183,11 +183,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587  # Port for SMTP
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sclopez7@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjph kgoq zndd ibpx'
-DEFAULT_FROM_EMAIL = 'sclopez7@gmail.com'
